40631336507120f664273aead5857c24db1e5a48
MariaSelhorst <madusel@hotmail.com>
2024-04-11 13:58:28 -0300

    finalizado

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const sequelize = require('sequelize');
 
 const database = new sequelize('Paintopia', 'paint', '12435901', 
 {
-    dialect: 'mssql', host:'localhost', port: 57191
+    dialect: 'mssql', host:'localhost', port: 1433
 });
 database.sync();
 
